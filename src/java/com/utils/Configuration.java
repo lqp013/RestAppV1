7f33e522e1f039a0e860fbdb4bbfa9effdb2ab78
Luis Quintano <149412452+Bigg69Cobra@users.noreply.github.com>
2024-01-22 16:26:42 +0100

    Changing final variable driver

--- src/java/com/utils/Configuration.java
+++ src/java/com/utils/Configuration.java
@@ -10,8 +10,8 @@ package com.utils;
  * @author Luis Quintano
  */
 public class Configuration {
-    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    public static final String URLCONNECTION = "jdbc:mysql://localhost:3306/dev";
+    public static final String DRIVER = "org.mariadb.jdbc.Driver";
+    public static final String URLCONNECTION = "jdbc:mariadb://localhost:3306/dev";
     public static final String USER = "root";
     public static final String PASSW = "root";
 }
