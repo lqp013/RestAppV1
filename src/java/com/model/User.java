f2f48603e3610c898448eba8baba7b21ede0080c
Luis Quintano <149412452+Bigg69Cobra@users.noreply.github.com>
2024-02-04 14:18:17 +0100

    solving errors

--- src/java/com/model/User.java
+++ src/java/com/model/User.java
@@ -11,23 +11,14 @@ import jakarta.xml.bind.annotation.XmlRootElement;
  *
  * @author Luis Quintano
  */
-//@Entity  //define que es una entidad dentro de una database.
-//@Table(name = "estudiantes")  //para poner el nombre de la Tabla en vez de como el nombre de la clase se lo ponemos nosotros.
+
 @XmlRootElement
 public class User {
-//    @Id  //siempre tiene que estar encima del atributo que quieras que sea el id (Primary Key).
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)  //para que se auto incremente solo.
-//    @Column(name = "id")  //para que la columna de la tabla se llame como le digas.
     private String id;
-    
-//    @Column(name = "nombre")  //para que la columna de la tabla se llame como le digas.
     private String name;
-    
-//    @Column(name = "rol")  //para que la columna de la tabla se llame como le digas.
     private String rol;
-    
-//    @Column(name = "yearbirth")  //para que la columna de la tabla se llame como le digas.
     private int yearbirth;
+    
     
     //constructores
     public User() {
@@ -40,11 +31,13 @@ public class User {
         this.yearbirth = yearBirth;
     }
     
+    
     //toString
     @Override
     public String toString() {
         return "User{" + "id=" + id + ", name=" + name + ", rol=" + rol + ", yearBirth=" + yearbirth + '}';
     }
+    
     
     //getters and setters
     public String getId() {
